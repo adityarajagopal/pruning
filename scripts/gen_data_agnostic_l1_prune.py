d2343a20d8fb16f1cf618532c82cc0a965be6cfb
adityarajagopal <ar4414@ic.ac.uk>
2020-03-03 21:50:22 +0000

    added new script to generate files for tx2 timing

--- scripts/gen_data_agnostic_l1_prune.py
+++ scripts/gen_data_agnostic_l1_prune.py
@@ -11,48 +11,44 @@ sub_classes = ["large_man-made_outdoor_things large_natural_outdoor_scenes vehic
 # sub_classes = [""]
 config = cp.ConfigParser()
 
-# base = '/home/ar4414/pytorch_training/src/ar4414/pruning/'
-base = '/home/nvidia/ar4414/pytorch_training/src/ar4414/pruning'
-
-configPath = os.path.join(base, 'configs')
-runFileBase = os.path.join(base, 'scripts')
-# cpRoot = os.path.join(base, 'logs/{}/cifar100/{}/data_agnostic_l1_prune')
+configPath = '/home/ar4414/pytorch_training/src/ar4414/pruning/configs/data_agnostic_l1_prune'
+runFileBase = '/home/ar4414/pytorch_training/src/ar4414/pruning/scripts/'
+cpRoot = "/home/ar4414/pytorch_training/src/ar4414/pruning/logs/{}/cifar100/{}/data_agnostic_l1_prune"
 
 cmd = 'mkdir -p ' + configPath
 subprocess.check_call(cmd, shell=True)
 
 for netCount, net in enumerate(nets):
     testCount = 0
-    configFile = '/home/ar4414/pytorch_training/src/ar4414/pruning/configs/' + str(net) + '.ini' 
+    configFile = '/home/ar4414/pytorch_training/src/ar4414/pruning/configs/' + str(net) + '.ini'
     config.read(configFile)
             
     repeats = 1
-    # gpu = "2" if (net == 'mobilenetv2' or net == 'resnet') else "3"
-    gpu = "0" 
+    gpu = "2" if (net == 'mobilenetv2' or net == 'resnet') else "3"
     runFile = os.path.join(runFileBase, 'run_{}.sh'.format(gpu))
     
-    config['training_hyperparameters']['print_only'] = "True"
+    config['training_hyperparameters']['print_only'] = "False"
     
     config['pytorch_parameters']['gpu_id'] = gpu
     config['pytorch_parameters']['resume'] = "False"
     config['pytorch_parameters']['branch'] = "False"
     config['pytorch_parameters']['evaluate'] = "False"
-    config['pytorch_parameters']['pretrained'] = "/home/nvidia/ar4414/pretrained/{}/cifar100.pth.tar".format(net)
     
     config['pruning_hyperparameters']['get_gops'] = "False"
     config['pruning_hyperparameters']['inference_gops'] = "False"
+    config['pruning_hyperparameters']['change_in_rank'] = "False"
     
     config['pruning_hyperparameters']['prune_filters'] = "True"
     config['pruning_hyperparameters']['finetune'] = "True"
     config['pruning_hyperparameters']['static'] = "True"
     config['pruning_hyperparameters']['retrain'] = "False"
     config['pruning_hyperparameters']['finetune_budget'] = "30"
-    config['pruning_hyperparameters']['prune_after'] = "5"
+    config['pruning_hyperparameters']['prune_after'] = "0"
 
     for ssCount, ss in enumerate(subset):
         config['pruning_hyperparameters']['sub_name'] = ss
         config['pruning_hyperparameters']['sub_classes'] = sub_classes[ssCount]
-        # config['pytorch_parameters']['checkpoint_path'] = cpRoot.format(net, ss) 
+        config['pytorch_parameters']['checkpoint_path'] = cpRoot.format(net, ss) 
 
         for ppCount, pp in enumerate(pruningPercs):
             config['pruning_hyperparameters']['pruning_perc'] = str(pp)
